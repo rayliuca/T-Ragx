548cf0527617f2e90a523343b4057be8c40d5093
Ray Liu <ray@rayliu.ca>
2024-03-02 19:31:04 -0700

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='t_ragx',
     package_dir={"": "src"},
     packages=find_packages("src"),
-    version='0.0.2',
+    version='0.0.3',
     description='Enhancing translation with RAG-powered large language models',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
