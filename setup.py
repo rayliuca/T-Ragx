cd210318ca6d6b225ed685270a9c0120f9675767
Ray Liu <ray@rayliu.ca>
2024-03-02 20:00:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,16 @@ setup(
     name='t_ragx',
     package_dir={"": "src"},
     packages=find_packages("src"),
-    version='0.0.3',
+    version='0.0.4',
+    url="https://github.com/rayliuca/T-Ragx",
     description='Enhancing translation with RAG-powered large language models',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Ray Liu',
     email='ray@rayliu.ca',
     license='MIT',
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/rayliuca/T-Ragx/issues",
+        "Source": "https://github.com/rayliuca/T-Ragx",
+    },
 )
