6d6755b74907f26209e71077b70fd19117c636f0
Ray Liu <ray@rayliu.ca>
2024-12-11 18:55:25 -0700

    Update constants.py

--- src/t_ragx/processors/constants.py
+++ src/t_ragx/processors/constants.py
@@ -1,3 +1,3 @@
-DEFAULT_GLOSSARY_PARQUET_FOLDER = "https://l8u0.c18.e2-1.dev/t-ragx-public/glossary"
+DEFAULT_GLOSSARY_PARQUET_FOLDER = "https://t-ragx-public.s3.us-west-004.backblazeb2.com/glossary"
 
 DEFAULT_MEMORY_INDEX = "translation_memory"
